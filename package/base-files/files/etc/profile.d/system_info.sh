8a94b37cde544358f05e12b7de3de598d61ccab2
hyj <huangyanjie@x-speed.com.cn>
2025-01-03 07:20:15 +0000

    add /etc/profile.d sh for login
    Signed-off-by: hyj <huangyanjie@x-speed.com.cn>

---
+++ package/base-files/files/etc/profile.d/system_info.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+display_system_info() {
+	echo
+	echo "        Basic System Information:"
+	echo "        ------------------------------------------------------"
+	echo "        Current Time   : $(date)"
+	echo "        System Version : $(cat /etc/os-release | grep -w 'PRETTY_NAME' | cut -d= -f2 | tr -d '"')"
+	echo "        Kernel Version : $(uname -r)"
+	echo "        HW Machine     : $(uname -m)"
+	echo "        Memory         : $(free -h | awk '/^Mem:/ { print $3 "/" $2 }')"
+	echo
+	echo "        Disk Usage Ranking:"
+	echo "        ------------------------------------------------------"
+	df -h | grep -vE 'tmpfs|devtmpfs|shm' | awk '{printf "\t%-12s %-6s %-6s %-11s %-6s %-9s\n",$1 ,$2, $3, $4 ,$5, $6}'
+	echo
+	echo "        NetWork Information:"
+	echo "        ------------------------------------------------------"
+	a=$(ifconfig -a | grep -E '^[a-z]' | awk '{print $1}' | grep -v 'lo')
+	b=$(ifconfig -a | grep 'HWaddr' | awk '{print $NF}')
+	i=0
+	for iface in $a; do
+		IP=$(ifconfig "$iface" | grep 'inet addr' | awk '{print $2}')
+		current_hwaddr=$(echo "$b" | sed -n "$((i + 1))p")
+		echo "$iface $current_hwaddr $IP" | awk '{printf "\t%-12s %-20s %-18s\n",$1 ,$2 ,$3}'
+		i=$((i + 1))
+	done
+	echo
+}
+
+display_system_info
