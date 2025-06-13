b5e95b15ac2c5120e5a3e6ae1173d4c8ed7bb9c8
hyj <huangyanjie@x-speed.com.cn>
2025-06-13 09:03:12 +0800

    Added support for the D3000 kernel
    Signed-off-by: hyj <huangyanjie@x-speed.com.cn>

---
+++ target/linux/xspeed/linux419246_d3000/target.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 Dustin <huangyanjie@x-speed.com.cn>
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+ARCH:=aarch64
+BOARDNAME:=Phytium D3000 Board (ARM64)
+KERNELNAME:=Image
+
+define Target/Description
+	Build firmware image for Xspeed Phytium D3000 Board.
+endef
